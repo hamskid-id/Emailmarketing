6e48a20e3de3fe821cd947227d4799e51eb45b67
princessvincent <priscavincent2018@gmail.com>
2023-02-07 18:00:05 -0800

    apis

--- app/Http/Controllers/EmailmarketingController.php
+++ app/Http/Controllers/EmailmarketingController.php
@@ -13,7 +13,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Session;
 
 class EmailmarketingController extends Controller
 {
@@ -133,7 +132,7 @@ class EmailmarketingController extends Controller
         $user->email = $request->email;
         $user->business_id = Auth::user()->business_id;
         $user->password = Hash::make($request->email);
-         $user->save();
+        $user->save();
 
         $invited = new inviteduser();
         $invited->user_id = Auth::user()->id;
@@ -148,7 +147,7 @@ class EmailmarketingController extends Controller
                 . 'login to your account using the following details',
                 'email' => $request->email,
                 'password' => $request->email,
-                'dashboard-link' => 'http://localhost:8000/home/'.Auth::user()->id
+                'dashboard-link' => 'http://localhost:8000/home/' . Auth::user()->id,
             ];
 
             Mail::to($request->email)->send(new sendemails($mailData));
@@ -182,7 +181,7 @@ class EmailmarketingController extends Controller
 
     //  public function visitaact($id)
     // {
-        
+
     //     $user = User::find($id)->first();
     //     //   dd($user->id);
     //     return view('dashboard', compact('user'));
@@ -195,11 +194,273 @@ class EmailmarketingController extends Controller
 
     //     Auth::logout();
     //     //  dd($request->password);
-        
+
     //     if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password])){
     //         return redirect('/home');
     //     }else{
     //         return redirect()->back()->with('success', 'you cant login');
     //     }
     // }
+
+    //for apis
+    public function createtags(Request $request)
+    {
+        // if (Auth::check()) {
+            $request->validate([
+                'name' => 'required',
+            ]);
+
+            $tag = new tags();
+            $tag->business_id = Auth::user()->business_id;
+            $tag->name = $request->name;
+            $tag->created_by = Auth::user()->id;
+            $tag->save();
+            if ($tag->save()) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Tag has been created successfully!',
+
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Unable to create tag',
+
+                ]);
+            }
+        // }else{
+        //     return response()->json([
+        //         'status' => false,
+        //         'message' => 'Unauthorized',
+        //     ]);
+        // }
+    }
+
+    public function viewtags()
+    {
+        if (Auth::check()) {
+            $tag = tags::where('business_id', auth::user()->business_id)->with("user")->get();
+            return response()->json([
+                'status' => true,
+                'message' => $tag,
+            ]);
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+    }
+
+    public function addsubscrib(Request $request)
+    {
+        if(Auth::check()){
+            $request->validate([
+                'email' => 'required|email|unique:subscribers',
+                'fname' => ['required'],
+                'lname' => ['required'],
+                'country' => ['required'],
+                'state' => ['required'],
+                'phone' => 'required|min:11',
+                'dob' => ['required'],
+                'tag' => ['required'],
+            ]);
+    
+            $subscrib = new subscriber();
+            $subscrib->business_id = auth::user()->business_id;
+            $subscrib->email = $request->email;
+            $subscrib->fname = $request->fname;
+            $subscrib->lname = $request->lname;
+            $subscrib->country = $request->country;
+            $subscrib->state = $request->state;
+            $subscrib->phone = $request->phone;
+            $subscrib->dob = $request->dob;
+            $subscrib->tag = $request->tag;
+            $subscrib->save();
+            if ($subscrib->save()) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Subscriber created successfully',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Unable to create Subscriber',
+                ]);
+            }
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+        
+
+    }
+
+    public function viewsubscribers()
+    {
+        if (Auth::check()) {
+            $subscrib = subscriber::where('business_id', Auth::user()->business_id)->get();
+            return response()->json([
+                'status' => true,
+                'message' => $subscrib,
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+    }
+
+    public function createcampaigns(Request $request)
+    {
+        if (Auth::check()) {
+            $request->validate([
+                'title' => 'required',
+                'recipient' => 'required',
+                'from' => 'required',
+                'subject' => 'required',
+                'content' => 'required',
+            ]);
+
+            $camp = new campaign();
+            $camp->business_id = auth::user()->business_id;
+            $camp->title = $request->title;
+            $camp->recipient = $request->recipient;
+            $camp->from = $request->from;
+            $camp->subject = $request->subject;
+            $camp->content = $request->content;
+            $camp->save();
+            if ($camp->save()) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Campgn created Successully!',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Unable to create campaign',
+                ]);
+            }
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+    }
+
+    public function viewcamps()
+    {
+        if (Auth::check()) {
+            $camp = campaign::where('business_id', auth::user()->business_id)->get();
+            return response()->json([
+                'status' => true,
+                'message' => $camp,
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+    }
+
+    public function inviteusers(Request $request)
+    {
+        if(Auth::check()){
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required',
+            ]);
+    
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->business_id = Auth::user()->business_id;
+            $user->password = Hash::make($request->email);
+            $user->save();
+    
+            $invited = new inviteduser();
+            $invited->user_id = Auth::user()->id;
+            $invited->name = $request->name;
+            $invited->email = $request->email;
+            // $invited->save();
+    
+            if ($invited->save()) {
+    
+                $mailData = [
+                    'content' => 'You have been Invited to collaborate with a friend in our workspace by' . ' ' . Auth::user()->name . ' '
+                    . 'login to your account using the following details',
+                    'email' => $request->email,
+                    'password' => $request->email,
+                    'dashboard-link' => 'http://localhost:8000/home/' . Auth::user()->id,
+                ];
+    
+                Mail::to($request->email)->send(new sendemails($mailData));
+                return response()->json([
+                    'status' => true,
+                    'message' => 'You have Successfully Invited' . ' ' . $request->email . ' ' . 'To your account and we have sent them an email notifying Them of your invitation!!',
+                ]);
+            }
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+       
+    }
+    public function collaborations()
+    {
+        if(Auth::check()){
+            $invite = inviteduser::where('email', Auth::user()->email)->first();
+            if ($invite) {
+                $user = User::where('id', $invite->user_id)->get();
+                return response()->json([
+                    'status' => true,
+                    'message' => $user,
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'success' => 'No Account have invited you to Collaborate with them!!',
+                ]);
+            }
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+       
+    }
+
+    public function collaborator()
+    {
+        if (Auth::check()) {
+            $usersinvited = inviteduser::where('user_id', Auth::user()->id)->get();
+            if ($usersinvited) {
+
+                return response()->json([
+                    'status' => true,
+                    'message' => $usersinvited,
+                ]);
+
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'You have not invited any Account to collaborate with you',
+                ]);
+            }
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ]);
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EmailmarketingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,41 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('dashboard/{id}', function () {
+//     return view('dashboard');
+// });
+
+// Route::get('addtag', function () {
+//     return view('emailmarketing.addtags');
+// });
+
+// Route::get('adduser', function () {
+//     return view('emailmarketing.adduser');
+// });
+
+// Route::get('addcamp', function () {
+//     return view('emailmarketing.createcampaign');
+// });
+
+Route::post('forgetpas', [UserController::class, 'forgetpassword']);
+Route::post('registering', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+// Route::get('viewtags', [EmailmarketingController::class, 'viewtags'])->name('viewtags');
+
+
+Route::group(['middleware' => 'auth:sanctum'], function (){
+    Route::post('changepassword', [UserController::class, 'changepass']);
+    Route::post('createtags', [EmailmarketingController::class, 'createtags'])->name('createtags');
+    Route::post('addsubscrib', [EmailmarketingController::class, 'addsubscrib'])->name('addsubscrib');
+    // Route::post('createsubscriber', [EmailmarketingController::class, 'createsubscriber'])->name('createsubscriber');
+    Route::get('viewtags', [EmailmarketingController::class, 'viewtags'])->name('viewtags');
+    Route::get('viewsubscrib', [EmailmarketingController::class, 'viewsubscribers'])->name('viewsubscrib');
+    Route::post('createcampaigns', [emailmarketingController::class, 'createcampaigns'])->name('createcampaigns');
+    Route::get('viewcamps', [emailmarketingController::class, 'viewcamps'])->name('viewcamps');
+    Route::post('inviteusers', [EmailmarketingController::class, 'inviteusers'])->name('inviteusers');
+    Route::get('collaborations', [EmailmarketingController::class, 'collaborations'])->name('collaborations');
+    Route::get('collaborators', [EmailmarketingController::class, 'collaborator'])->name('collaborators');
+    
+});
+
