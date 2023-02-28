e3a134d4a1bf7d9c137c2e4d32747eb0f1af18c0
princessvincent <priscavincent2018@gmail.com>
2023-02-27 16:50:49 -0800

    update

--- routes/api.php
+++ routes/api.php
@@ -48,12 +48,32 @@ Route::group(['middleware' => 'auth:sanctum'], function (){
     Route::post('addsubscrib', [EmailmarketingController::class, 'addsubscrib'])->name('addsubscrib');
     // Route::post('createsubscriber', [EmailmarketingController::class, 'createsubscriber'])->name('createsubscriber');
     Route::get('viewtags', [EmailmarketingController::class, 'viewtags'])->name('viewtags');
+    Route::get('edittags/{id}', [EmailmarketingController::class, 'edittags'])->name('edittags');
+    Route::put('updatetags/{id}', [EmailmarketingController::class, 'updatetags'])->name('updatetags');
     Route::get('viewsubscrib', [EmailmarketingController::class, 'viewsubscribers'])->name('viewsubscrib');
+    Route::get('unsubscribe', [EmailmarketingController::class, 'viewunsubscribers'])->name('unsubscribe');
     Route::post('createcampaigns', [emailmarketingController::class, 'createcampaigns'])->name('createcampaigns');
     Route::get('viewcamps', [emailmarketingController::class, 'viewcamps'])->name('viewcamps');
     Route::post('inviteusers', [EmailmarketingController::class, 'inviteusers'])->name('inviteusers');
     Route::get('collaborations', [EmailmarketingController::class, 'collaborations'])->name('collaborations');
     Route::get('collaborators', [EmailmarketingController::class, 'collaborator'])->name('collaborators');
+    Route::post('create_spamreport', [EmailmarketingController::class, 'create_spamreport'])->name('create_spamreport');
+    Route::get('list_spamreport', [EmailmarketingController::class, 'list_spamreport'])->name('list_spamreport');
+    Route::post('blasklisted', [EmailmarketingController::class, 'blasklisted'])->name('blasklisted');
+    Route::get('viewblacklisted', [EmailmarketingController::class, 'viewblacklisted'])->name('viewblacklisted');
+    Route::get('totalsubscriber', [EmailmarketingController::class, 'totalsubscriber'])->name('totalsubscriber');
+    // for activities log
+    Route::post('addactivitylog', [EmailmarketingController::class, 'addactivitylog'])->name('addactivitylog');
+    Route::get('viewactivitylog', [EmailmarketingController::class, 'viewactivitylog'])->name('viewactivitylog');
+    //for api for general and user template
+    Route::post('generaltemp', [EmailmarketingController::class, 'generaltemp'])->name('generaltemp');
+    Route::post('usertemplate', [EmailmarketingController::class, 'usertemplate'])->name('usertemplate');
+    Route::get('viewusertemp', [EmailmarketingController::class, 'viewusertemp'])->name('viewusertemp');
+    //for list subscriber email
+    Route::get('subscribermail', [EmailmarketingController::class, 'subscribermail'])->name('subscribermail');
     
+
+
+
 });
 
