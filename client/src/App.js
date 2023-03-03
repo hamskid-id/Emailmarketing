f1b7bb8936c8675ecefcb39c5d4eee9c3ecc11e5
hamzat <lawalhamzat27@gmail.com>
2023-03-03 13:32:55 +0100

    added gh-pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import {HashRouter as Router, Routes, Route, } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { ToastContainer } from 'react-toastify';
