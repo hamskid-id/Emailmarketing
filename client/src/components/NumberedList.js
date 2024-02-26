98e02e531474bb7ca0b4f5ca91c3fe3881bf8816
hamskid-id <lawalhamzat27@gmail.com>
2024-02-26 08:57:10 -0800

    updated

--- client/src/components/NumberedList.js
+++ client/src/components/NumberedList.js
@@ -5,43 +5,9 @@ export const NumberedList =({
     response
     })=>{
         if(response){
-            const responseItems = response.split('\n');
-            const isList = responseItems.length > 1;
             return (
                 <div className="response-container">
-                  {isList ? (
-                    // Render numbered list if multiple lines
-                    <div>
-                        <div>
-                        {
-                            responseItems.map((item, index) => {
-                                const text = item.trim().slice(2);
-                                return(
-                                    <p 
-                                        key={index} 
-                                        className="numbered-list-item"
-                                    >
-                                        <div>{text}</div> {/* Use slicing to remove numbering */}
-                                    </p>
-                                )
-                                }
-                            )
-                        }
-                        </div>
-                        <div>
-                        <AiFillCopy 
-                                size="1.5rem"
-                                color="gainsboro"                                
-                                onClick={() => {
-                                    navigator.clipboard.writeText(response)
-                                    toast("Copied to Clippard")
-                                }}
-                            />
-                        </div>
-                    </div>
-                  ) : (
-                    // Render single line if not a list
-                    <div>
+                  <div>
                         <p className="single-line-response">{response}</p>
                         <div>
                             <AiFillCopy 
@@ -50,12 +16,11 @@ export const NumberedList =({
                                 className="cursor-pointer"                              
                                 onClick={() => {
                                     navigator.clipboard.writeText(response)
-                                    toast("Copied to Clippard")
+                                    toast("Copied to Clipboard")
                                 }}
                             />
                         </div>
                     </div>
-                  )}
                 </div>
               );
         }
